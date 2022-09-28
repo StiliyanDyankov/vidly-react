9da9b97e06d3cfcd00a4e437241a6e9abdceb03b
Stiliyan Dyankov <stiliandiankov0704@gmail.com>
2022-09-28 12:04:33 +0300

    Adding routing tut

--- vidly/src/App.js
+++ vidly/src/App.js
@@ -1,11 +1,29 @@
 import "./App.css";
+import { Route, Switch, Redirect } from "react-router-dom";
+import React from "react";
 import Movies from "./components/movies";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
+import NavBar from "./components/navBar";
+import MovieForm from './components/movieForm';
 
 function App() {
     return (
-        <main className="container">
-            <Movies />
-        </main>
+        <React.Fragment>
+            <NavBar />
+            <div className="m-4">
+                <Switch>
+                    <Route path='/movies/:id' component={MovieForm}/>
+                    <Route path="/movies" component={Movies} />
+                    <Route path="/customers" component={Customers} />
+                    <Route path="/rentals" component={Rentals} />
+                    <Route path="/not-found" component={NotFound} />
+                    <Redirect from="/" exact to="/movies" />
+                    <Redirect to="/not-found" />
+                </Switch>
+            </div>
+        </React.Fragment>
     );
 }
 
